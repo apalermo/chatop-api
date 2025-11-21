51b52a76064650e8e06749a9436f57862147b54b
apalermo <anthony.palermo@laposte.net>
2025-11-21 11:12:17 +0100

    feat: add Message entity and repository

---
+++ src/main/java/com/chatop/api/model/Message.java
@@ -0,0 +1,35 @@
+package com.chatop.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "rental_id")
+    private Long rentalId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(length = 2000)
+    private String message;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
