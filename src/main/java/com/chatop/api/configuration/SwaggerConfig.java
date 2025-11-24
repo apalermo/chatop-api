7b2182e058d35c9b9c9a3f16c5b0f2f1b0c735d5
apalermo <anthony.palermo@laposte.net>
2025-11-24 10:33:00 +0100

    chore: configure swagger UI with JWT security scheme

---
+++ src/main/java/com/chatop/api/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.chatop.api.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                // 1. On définit le schéma de sécurité (JWT)
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                // 2. On applique cette sécurité globalement à toute l'API
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
+}
